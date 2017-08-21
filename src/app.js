a8fc16768160ca2c33bdee6e9ef10a9707289bc4
bnoden <brandroid369@gmail.com>
2017-08-20 19:04:47 -0500

    '.'

--- src/app.js
+++ src/app.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 import { Header, Titles } from './components/common';
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyDqZNOU64hSZO-u6RzngrVRHosVonvNTD4',
+      authDomain: 'auth-47ef6.firebaseapp.com',
+      databaseURL: 'https://auth-47ef6.firebaseio.com',
+      projectId: 'auth-47ef6',
+      storageBucket: 'auth-47ef6.appspot.com',
+      messagingSenderId: '72128811864'
+    });
+  }
+
   render() {
     const { appTitle = 'App Title', headerTitle = 'Header Title' } = { Titles };
 
